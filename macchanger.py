d968fa01fb6ef4e91cf907049632c4d7e7ec7506
Rishav Sharma <33522557+Sparta0025@users.noreply.github.com>
2020-02-19 13:02:24 -0500

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -1,14 +1,36 @@
 import subprocess
 import argparse
 
-parser=argparse.ArgumentParser()
-parser.add_argument('-i','--interface',metavar='',help='provide interface name for following operation')
-parser.add_argument('-m','--mac',metavar='',help='provide new mac address for following operation')
-args=parser.parse_args()
-h=args.interface
-print("[+] changing mac address to ",args.mac)
-subprocess.run(["ifconfig",args.interface,"down"])
-subprocess.run(["ifconfig",args.interface,"hw","ether",args.mac])
-subprocess.run(["ifconfig",args.interface,"up"])
-print("[+]MAC ADDRESS changed successfully(^_^)!!!")
+
+def arguments():
+	parser=argparse.ArgumentParser()
+	parser.add_argument('-i','--interface',metavar='',help='provide interface name for following operation')
+	parser.add_argument('-m','--mac',metavar='',help='provide new mac address for following operation')
+	arg=parser.parse_args()
+	
+	if not arg.interface :
+		parser.error("ERROR!!!!enter interface:: use --help for more info\n")
+		
+		
+	elif not arg.mac:
+		parser.error("ERROR!!!!enter interface:: use --help for more info\n")
+		
+		
+	
+		
+
+	print("[+] changing mac address to ",arg.mac)
+	return arg
+
+
+
+def exec(interface,mac):
+	subprocess.run(["ifconfig",interface,"down"])
+	subprocess.run(["ifconfig",interface,"hw","ether",mac])
+	subprocess.run(["ifconfig",interface,"up"])
+	print("[+]MAC ADDRESS changed successfully(^_^)!!!")
+
+args=arguments()
+exec(args.interface, args.mac)
+
 
